bf525b126a791c57853c89cd8308e2d249190207
Gonzalo Ferreyra <gonferreyra@gmail.com>
2023-02-23 17:53:08 -0300

    custom hook authStatus

---
+++ src/hooks/useAuthStatus.tsx
@@ -0,0 +1,20 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+
+function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [checkingStatus, setCheckingStatus] = useState<boolean>(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  }, []);
+  return { loggedIn, checkingStatus };
+}
+
+export default useAuthStatus;
